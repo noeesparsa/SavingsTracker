7a9e053050d04fdfda6438f93193747da2fda669
Noé <noe.esp@hotmail.fr>
2026-04-01 12:00:21 +0200

    chore: add othe account api

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,10 @@
 const apiUrlByUser: Record<string, string> = {
   "noe.esp@hotmail.fr":
     "https://script.google.com/macros/s/AKfycbwzNwxM1DKmZFYl58e2HpL-x_u6N2rfJljT-My_X5iCiqRHgLANUU4Q9T92gZVzi2Yf/exec",
+  "lea.alami@gmail.com":
+    "https://script.google.com/macros/s/AKfycbwfZAplyxR4xa8lOT5f5B7jrJ8HwcjuRREYdF7DxBkERmt5Hqw-GKIeRB3DzLhnBwCuww/exec",
+  "esparsapaul@gmail.com":
+    "https://script.google.com/macros/s/AKfycbzau1P3QOynvrs_9V2GDqs97xkjqK2ZGDXEkbOmox4UbRxVCURKyq2c6wYgb5SelptpOg/exec",
 };
 
 export { apiUrlByUser };
